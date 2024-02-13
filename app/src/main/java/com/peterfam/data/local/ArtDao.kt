161d2d1af5f6623e8a624c2d8524975b913a22f1
peterfam <peter.fam@advansys-esc.com>
2024-02-13 19:48:42 +0200

    implement app local database

---
+++ app/src/main/java/com/peterfam/data/local/ArtDao.kt
@@ -0,0 +1,20 @@
+package com.peterfam.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.peterfam.data.model.Art
+
+interface ArtDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArt(art: Art)
+    @Delete
+    suspend fun deleteArt(art: Art)
+
+
+    @Query("SELECT * FROM arts")
+    fun observeArts(): LiveData<List<Art>>
+
+}
---
+++ app/src/main/java/com/peterfam/data/model/Art.kt
@@ -0,0 +1,14 @@
+package com.peterfam.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "arts")
+data class Art(
+    var name: String,
+    var artistName: String,
+    var year: Int,
+    var imageURL: String,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int
+)
